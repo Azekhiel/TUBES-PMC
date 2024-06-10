505e21fac7010f461ea924a919d75523427e5c35
William <115348219+Azekhiel@users.noreply.github.com>
2024-06-10 17:41:30 +0700

    Update baca.h

--- baca.h
+++ baca.h
@@ -95,13 +95,6 @@ void bacaDataPasien(char *filename) {
 }
 
 void bacaRiwayatDatang(char *filename) {
-    // int no;
-    // char tangga[255];
-    // char id[255];
-    // char diagnosis[255];
-    // char tindakan[255];
-    // char kontrol[255];
-    // int biaya;
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
         printf("File tidak ada %s\n", filename);
@@ -150,6 +143,8 @@ void bacaRiwayatDatang(char *filename) {
     fclose(file);
 }
 
+
+
 void bacaBiayaTindakan(char *filename) {
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
@@ -188,7 +183,7 @@ void bacaBiayaTindakan(char *filename) {
 }
 
 
-// buat cek isi linked list sesuain aj nanti sama data strukturny
+//buat cek isi linked list sesuain aj nanti sama data strukturny
 // void printLinkedList() {
 //     biayaTindakan *current = head_biayaTindakan;
 //     while (current != NULL) {
@@ -198,23 +193,270 @@ void bacaBiayaTindakan(char *filename) {
 //     }
 // }
 
-void rapikanFormat(){
-    char temp[3];
+void rapikanFormatDataPasien(){
+    char temp_tanggal[100];
+    char temp_tahun[10];
+    char temp_bulan[16];
+    char tahun[20];
     dataPasien *current = head_dataPasien;
     while (current!= NULL){
         if (strstr(current->tanggal_lahir,"Jan")!=NULL){
-            temp[0] = current->tanggal_lahir [0];
-            temp[1] = current->tanggal_lahir [1];
-
+            strcpy(temp_bulan,"Januari ");
         }
+        else if (strstr(current->tanggal_lahir,"Feb")!=NULL){
+            strcpy(temp_bulan,"Februari ");
+        }
+
+        else if (strstr(current->tanggal_lahir,"Mar")!=NULL){
+            strcpy(temp_bulan,"Maret ");
+        }
+
+        else if (strstr(current->tanggal_lahir,"Apr")!=NULL){
+            strcpy(temp_bulan,"April ");
+        }
+
+        else if (strstr(current->tanggal_lahir,"Mei")!=NULL){
+            strcpy(temp_bulan,"Mei ");
+        }
+
+        else if (strstr(current->tanggal_lahir,"Jun")!=NULL){
+            strcpy(temp_bulan,"Juni ");
+        }
+
+        else if (strstr(current->tanggal_lahir,"Jul")!=NULL){
+            strcpy(temp_bulan,"Juli ");
+        }
+
+        else if (strstr(current->tanggal_lahir,"Agu")!=NULL){
+            strcpy(temp_bulan,"Agustus ");
+        }
+
+        else if (strstr(current->tanggal_lahir,"Sep")!=NULL){
+            strcpy(temp_bulan,"September ");
+        }
+
+        else if (strstr(current->tanggal_lahir,"Okt")!=NULL){
+            strcpy(temp_bulan,"Oktober ");
+        }
+
+        else if (strstr(current->tanggal_lahir,"Nov")!=NULL){
+            strcpy(temp_bulan,"November ");
+        }
+
+        else if (strstr(current->tanggal_lahir,"Des")!=NULL){
+            strcpy(temp_bulan,"Desember ");
+        }
+            temp_tanggal[0] = current->tanggal_lahir [0];
+            temp_tanggal[1] = current->tanggal_lahir [1];
+            temp_tanggal[2] = ' ';
+            temp_tanggal[3] = '\0'; // Null-terminate the string
+
+            if (strrchr(current->tanggal_lahir, '-')!=NULL){
+                strcpy(temp_tahun,strrchr(current->tanggal_lahir,'-') +1);
+                if (atoi(temp_tahun)<=24){
+                    strcpy(tahun,"20");
+                    strcat(tahun,temp_tahun);                }
+                else{
+                    strcpy(tahun,"19");
+                    strcat(tahun,temp_tahun);
+                 }                
+            }
+            else if (strrchr(current->tanggal_lahir,' ')!=NULL){
+                strcpy(temp_tahun,strrchr(current->tanggal_lahir,' ') +1);
+            }        
+
+            strcat(temp_tanggal,temp_bulan);
+            strcat(temp_tanggal,tahun);
+            strcpy(current->tanggal_lahir, temp_tanggal);  
+
+            current = current->next;
     }
 
 }
-//kalo mau tes fungsiny sendiri jangan lupa ganti main()
-int baca() {
+
+
+void rapikanFormatRiwayatDatang_Tanggal(){
+    char temp_tanggal[100];
+    char temp_tahun[10];
+    char temp_bulan[16];
+    char tahun[20];
+    riwayatDatang *current = head_riwayatDatang;
+    while (current!= NULL){
+        if (strstr(current->tanggal,"Jan")!=NULL){
+            strcpy(temp_bulan,"Januari ");
+        }
+        else if (strstr(current->tanggal,"Feb")!=NULL){
+            strcpy(temp_bulan,"Februari ");
+        }
+
+        else if (strstr(current->tanggal,"Mar")!=NULL){
+            strcpy(temp_bulan,"Maret ");
+        }
+
+        else if (strstr(current->tanggal,"Apr")!=NULL){
+            strcpy(temp_bulan,"April ");
+        }
+
+        else if (strstr(current->tanggal,"Mei")!=NULL){
+            strcpy(temp_bulan,"Mei ");
+        }
+
+        else if (strstr(current->tanggal,"Jun")!=NULL){
+            strcpy(temp_bulan,"Juni ");
+        }
+
+        else if (strstr(current->tanggal,"Jul")!=NULL){
+            strcpy(temp_bulan,"Juli ");
+        }
+
+        else if (strstr(current->tanggal,"Agu")!=NULL){
+            strcpy(temp_bulan,"Agustus ");
+        }
+
+        else if (strstr(current->tanggal,"Sep")!=NULL){
+            strcpy(temp_bulan,"September ");
+        }
+
+        else if (strstr(current->tanggal,"Okt")!=NULL){
+            strcpy(temp_bulan,"Oktober ");
+        }
+
+        else if (strstr(current->tanggal,"Nov")!=NULL){
+            strcpy(temp_bulan,"November ");
+        }
+
+        else if (strstr(current->tanggal,"Des")!=NULL){
+            strcpy(temp_bulan,"Desember ");
+        }
+            temp_tanggal[0] = current->tanggal [0];
+            temp_tanggal[1] = current->tanggal [1];
+            temp_tanggal[2] = ' ';
+            temp_tanggal[3] = '\0'; // Null-terminate the string
+
+            if (strrchr(current->tanggal, '-')!=NULL){
+                strcpy(temp_tahun,strrchr(current->tanggal,'-') +1);
+                if (atoi(temp_tahun)<=24){
+                    strcpy(tahun,"20");
+                    strcat(tahun,temp_tahun);                }
+                else{
+                    strcpy(tahun,"19");
+                    strcat(tahun,temp_tahun);
+                 }                
+            }
+            else if (strrchr(current->tanggal,' ')!=NULL){
+                strcpy(temp_tahun,strrchr(current->tanggal,' ') +1);
+            }        
+
+            strcat(temp_tanggal,temp_bulan);
+            strcat(temp_tanggal,tahun);
+            strcpy(current->tanggal, temp_tanggal);  
+
+            current = current->next;
+    }
+
+}
+
+void rapikanFormatRiwayatDatang_Kontrol(){
+    char temp_kontrol[100];
+    char temp_tahun[10];
+    char temp_bulan[16];
+    char tahun[20];
+    riwayatDatang *current = head_riwayatDatang;
+    while (current!= NULL){
+        if (strstr(current->kontrol,"Jan")!=NULL){
+            strcpy(temp_bulan,"Januari ");
+        }
+        else if (strstr(current->kontrol,"Feb")!=NULL){
+            strcpy(temp_bulan,"Februari ");
+        }
+
+        else if (strstr(current->kontrol,"Mar")!=NULL){
+            strcpy(temp_bulan,"Maret ");
+        }
+
+        else if (strstr(current->kontrol,"Apr")!=NULL){
+            strcpy(temp_bulan,"April ");
+        }
+
+        else if (strstr(current->kontrol,"Mei")!=NULL){
+            strcpy(temp_bulan,"Mei ");
+        }
+
+        else if (strstr(current->kontrol,"Jun")!=NULL){
+            strcpy(temp_bulan,"Juni ");
+        }
+
+        else if (strstr(current->kontrol,"Jul")!=NULL){
+            strcpy(temp_bulan,"Juli ");
+        }
+
+        else if (strstr(current->kontrol,"Agu")!=NULL){
+            strcpy(temp_bulan,"Agustus ");
+        }
+
+        else if (strstr(current->kontrol,"Sep")!=NULL){
+            strcpy(temp_bulan,"September ");
+        }
+
+        else if (strstr(current->kontrol,"Okt")!=NULL){
+            strcpy(temp_bulan,"Oktober ");
+        }
+
+        else if (strstr(current->kontrol,"Nov")!=NULL){
+            strcpy(temp_bulan,"November ");
+        }
+
+        else if (strstr(current->kontrol,"Des")!=NULL){
+            strcpy(temp_bulan,"Desember ");
+        }
+            temp_kontrol[0] = current->kontrol [0];
+            temp_kontrol[1] = current->kontrol [1];
+            temp_kontrol[2] = ' ';
+            temp_kontrol[3] = '\0'; // Null-terminate the string
+
+            if (strrchr(current->kontrol, '-')!=NULL){
+                strcpy(temp_tahun,strrchr(current->kontrol,'-') +1);
+                if (atoi(temp_tahun)<=24){
+                    strcpy(tahun,"20");
+                    strcat(tahun,temp_tahun);                }
+                else{
+                    strcpy(tahun,"19");
+                    strcat(tahun,temp_tahun);
+                 }                
+            }
+            else if (strrchr(current->kontrol,' ')!=NULL){
+                strcpy(temp_tahun,strrchr(current->kontrol,' ') +1);
+            }        
+
+            strcat(temp_kontrol,temp_bulan);
+            strcat(temp_kontrol,tahun);
+            strcpy(current->kontrol, temp_kontrol);  
+
+            current = current->next;
+    }
+
+}
+
+
+void printDataPasien() {
+    printf("| No | Nama Lengkap     | Alamat             | Kota          | Tempat Lahir   | Tanggal Lahir | Umur | No BPJS    | ID        |\n");
+    printf("------------------------------------------------------------------------------------------------------------------------------\n");
+    dataPasien *current = head_dataPasien;
+    while (current != NULL) {
+        printf("| %-2d | %-15s | %-18s | %-13s | %-14s | %-13s | %-4d | %-10s | %-9s |\n",
+               current->no, current->nama, current->alamat, current->kota, current->tempat_lahir,
+               current->tanggal_lahir, current->umur, current->noBPJS, current->id);
+        current = current->next;
+    }
+}
+
+int main() {
     bacaDataPasien("DataPMC20232024 - Data Pasien.csv");
     bacaBiayaTindakan("DataPMC20232024 - Biaya Tindakan.csv");
     bacaRiwayatDatang("DataPMC20232024 - Riwayat Datang, Diag,, Tindakan.csv");
-    // printLinkedList();
+    rapikanFormatDataPasien();
+    rapikanFormatRiwayatDatang_Tanggal();
+    rapikanFormatRiwayatDatang_Kontrol();
+    // printDataPasien();
     return 0;
 }
