4ef627da753eb0831932cfa1da1eef838587b8c0
Vincentius7704 <113782660+Vincentius7704@users.noreply.github.com>
2024-06-16 12:25:39 +0700

    Fitur3

---
+++ fitur3.h
@@ -0,0 +1,12 @@
+#include "baca.h"
+
+void fitur3(){
+  char *input_id_pasien;
+  printf("Silahkan input ID pasien: ");
+  scanf("%s" , input_id_pasien);
+
+  //Mengprint data pasien pada dataPasien
+
+
+  //Mengprint riwayat diagnosis dengan format diagnosis -- tanggal dari riwayatDatanga 
+}
