8d1928025a445a62e8441f92c633250015c3e655
Vincentius7704 <113782660+Vincentius7704@users.noreply.github.com>
2024-06-17 10:03:00 +0700

    update file untuk william

--- gtktesting/app.c
+++ gtktesting/app.c
@@ -9,10 +9,15 @@ void on_info_pasien_pertahun_clicked(GtkWidget *widget, gpointer data);
 void on_tanggal_clicked(GtkWidget *widget, gpointer data);
 
 // Forward declarations for submenu actions
-void on_edit_clicked(GtkWidget *widget, gpointer data);
-void on_hapus_clicked(GtkWidget *widget, gpointer data);
-void on_cari_clicked(GtkWidget *widget, gpointer data);
-void on_tambah_clicked(GtkWidget *widget, gpointer data);
+void on_edit_data_pasien_edit_clicked(GtkWidget *widget, gpointer data);
+void on_edit_data_pasien_hapus_clicked(GtkWidget *widget, gpointer data);
+void on_edit_data_pasien_cari_clicked(GtkWidget *widget, gpointer data);
+void on_edit_data_pasien_tambah_clicked(GtkWidget *widget, gpointer data);
+
+void on_edit_riwayat_edit_clicked(GtkWidget *widget, gpointer data);
+void on_edit_riwayat_hapus_clicked(GtkWidget *widget, gpointer data);
+void on_edit_riwayat_cari_clicked(GtkWidget *widget, gpointer data);
+void on_edit_riwayat_tambah_clicked(GtkWidget *widget, gpointer data);
 
 // Forward declaration for common functions
 void on_back_to_main_menu(GtkWidget *widget, gpointer data);
@@ -94,11 +99,11 @@ void create_submenu(const char *title, GCallback edit_callback, GCallback hapus_
 }
 
 void on_edit_data_pasien_clicked(GtkWidget *widget, gpointer data) {
-    create_submenu("Edit Data Pasien", G_CALLBACK(on_edit_clicked), G_CALLBACK(on_hapus_clicked), G_CALLBACK(on_cari_clicked), G_CALLBACK(on_tambah_clicked));
+    create_submenu("Edit Data Pasien", G_CALLBACK(on_edit_data_pasien_edit_clicked), G_CALLBACK(on_edit_data_pasien_hapus_clicked), G_CALLBACK(on_edit_data_pasien_cari_clicked), G_CALLBACK(on_edit_data_pasien_tambah_clicked));
 }
 
 void on_edit_riwayat_clicked(GtkWidget *widget, gpointer data) {
-    create_submenu("Edit Riwayat", G_CALLBACK(on_edit_clicked), G_CALLBACK(on_hapus_clicked), G_CALLBACK(on_cari_clicked), G_CALLBACK(on_tambah_clicked));
+    create_submenu("Edit Riwayat", G_CALLBACK(on_edit_riwayat_edit_clicked), G_CALLBACK(on_edit_riwayat_hapus_clicked), G_CALLBACK(on_edit_riwayat_cari_clicked), G_CALLBACK(on_edit_riwayat_tambah_clicked));
 }
 
 void on_info_pasien_clicked(GtkWidget *widget, gpointer data) {
@@ -173,7 +178,40 @@ void on_tanggal_clicked(GtkWidget *widget, gpointer data) {
     gtk_widget_show_all(main_window);
 }
 
-void on_edit_clicked(GtkWidget *widget, gpointer data) {
+void on_edit_data_pasien_edit_clicked(GtkWidget *widget, gpointer data) {
+    clear_window();
+
+    GtkWidget *entry_id = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(entry_id), "Masukkan ID Pasien");
+    gtk_box_pack_start(GTK_BOX(main_box), entry_id, TRUE, TRUE, 0);
+
+    GtkWidget *entry_name = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(entry_name), "Masukkan Nama Baru");
+    gtk_box_pack_start(GTK_BOX(main_box), entry_name, TRUE, TRUE, 0);
+
+    GtkWidget *entry_address = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(entry_address), "Masukkan Alamat Baru");
+    gtk_box_pack_start(GTK_BOX(main_box), entry_address, TRUE, TRUE, 0);
+
+    GtkWidget *entry_birth_date = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(entry_birth_date), "Masukkan Tempat Tanggal Lahir Baru");
+    gtk_box_pack_start(GTK_BOX(main_box), entry_birth_date, TRUE, TRUE, 0);
+
+    GtkWidget *button = gtk_button_new_with_label("Submit");
+    g_signal_connect(button, "clicked", G_CALLBACK(on_back_to_main_menu), entry_id);
+    g_signal_connect(button, "clicked", G_CALLBACK(on_back_to_main_menu), entry_name);
+    g_signal_connect(button, "clicked", G_CALLBACK(on_back_to_main_menu), entry_address);
+    g_signal_connect(button, "clicked", G_CALLBACK(on_back_to_main_menu), entry_birth_date);
+    gtk_box_pack_start(GTK_BOX(main_box), button, TRUE, TRUE, 0);
+
+    GtkWidget *back_button = gtk_button_new_with_label("Kembali");
+    g_signal_connect(back_button, "clicked", G_CALLBACK(on_back_to_main_menu), NULL);
+    gtk_box_pack_start(GTK_BOX(main_box), back_button, TRUE, TRUE, 0);
+
+    gtk_widget_show_all(main_window);
+}
+
+void on_edit_data_pasien_hapus_clicked(GtkWidget *widget, gpointer data) {
     clear_window();
 
     GtkWidget *entry = gtk_entry_new();
@@ -184,14 +222,14 @@ void on_edit_clicked(GtkWidget *widget, gpointer data) {
     g_signal_connect(button, "clicked", G_CALLBACK(on_back_to_main_menu), entry);
     gtk_box_pack_start(GTK_BOX(main_box), button, TRUE, TRUE, 0);
 
-    button = gtk_button_new_with_label("Kembali");
-    g_signal_connect(button, "clicked", G_CALLBACK(on_back_to_main_menu), NULL);
-    gtk_box_pack_start(GTK_BOX(main_box), button, TRUE, TRUE, 0);
+    GtkWidget *back_button = gtk_button_new_with_label("Kembali");
+    g_signal_connect(back_button, "clicked", G_CALLBACK(on_back_to_main_menu), NULL);
+    gtk_box_pack_start(GTK_BOX(main_box), back_button, TRUE, TRUE, 0);
 
     gtk_widget_show_all(main_window);
 }
 
-void on_hapus_clicked(GtkWidget *widget, gpointer data) {
+void on_edit_data_pasien_cari_clicked(GtkWidget *widget, gpointer data) {
     clear_window();
 
     GtkWidget *entry = gtk_entry_new();
@@ -199,8 +237,11 @@ void on_hapus_clicked(GtkWidget *widget, gpointer data) {
     gtk_box_pack_start(GTK_BOX(main_box), entry, TRUE, TRUE, 0);
 
     GtkWidget *button = gtk_button_new_with_label("Submit");
-       g_signal_connect(button, "clicked", G_CALLBACK(on_back_to_main_menu), entry);
+    g_signal_connect(button, "clicked", G_CALLBACK(on_back_to_main_menu), entry);
     gtk_box_pack_start(GTK_BOX(main_box), button, TRUE, TRUE, 0);
+
+    GtkWidget *label = gtk_label_new("Data tidak ditemukan");
+    gtk_box_pack_start(GTK_BOX(main_box), label, TRUE, TRUE, 0);
 
     GtkWidget *back_button = gtk_button_new_with_label("Kembali");
     g_signal_connect(back_button, "clicked", G_CALLBACK(on_back_to_main_menu), NULL);
@@ -209,7 +250,7 @@ void on_hapus_clicked(GtkWidget *widget, gpointer data) {
     gtk_widget_show_all(main_window);
 }
 
-void on_cari_clicked(GtkWidget *widget, gpointer data) {
+void on_edit_data_pasien_tambah_clicked(GtkWidget *widget, gpointer data) {
     clear_window();
 
     GtkWidget *entry = gtk_entry_new();
@@ -227,7 +268,64 @@ void on_cari_clicked(GtkWidget *widget, gpointer data) {
     gtk_widget_show_all(main_window);
 }
 
-void on_tambah_clicked(GtkWidget *widget, gpointer data) {
+void on_edit_riwayat_edit_clicked(GtkWidget *widget, gpointer data) {
+    clear_window();
+
+    GtkWidget *entry = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(entry), "Masukkan ID");
+    gtk_box_pack_start(GTK_BOX(main_box), entry, TRUE, TRUE, 0);
+
+    GtkWidget *button = gtk_button_new_with_label("Submit");
+    g_signal_connect(button, "clicked", G_CALLBACK(on_back_to_main_menu), entry);
+    gtk_box_pack_start(GTK_BOX(main_box), button, TRUE, TRUE, 0);
+
+    GtkWidget *back_button = gtk_button_new_with_label("Kembali");
+    g_signal_connect(back_button, "clicked", G_CALLBACK(on_back_to_main_menu), NULL);
+    gtk_box_pack_start(GTK_BOX(main_box), back_button, TRUE, TRUE, 0);
+
+    gtk_widget_show_all(main_window);
+}
+
+void on_edit_riwayat_hapus_clicked(GtkWidget *widget, gpointer data) {
+    clear_window();
+
+    GtkWidget *entry = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(entry), "Masukkan ID");
+    gtk_box_pack_start(GTK_BOX(main_box), entry, TRUE, TRUE, 0);
+
+    GtkWidget *button = gtk_button_new_with_label("Submit");
+    g_signal_connect(button, "clicked", G_CALLBACK(on_back_to_main_menu), entry);
+    gtk_box_pack_start(GTK_BOX(main_box), button, TRUE, TRUE, 0);
+
+    GtkWidget *back_button = gtk_button_new_with_label("Kembali");
+    g_signal_connect(back_button, "clicked", G_CALLBACK(on_back_to_main_menu), NULL);
+    gtk_box_pack_start(GTK_BOX(main_box), back_button, TRUE, TRUE, 0);
+
+    gtk_widget_show_all(main_window);
+}
+
+void on_edit_riwayat_cari_clicked(GtkWidget *widget, gpointer data) {
+    clear_window();
+
+    GtkWidget *entry = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(entry), "Masukkan ID");
+    gtk_box_pack_start(GTK_BOX(main_box), entry, TRUE, TRUE, 0);
+
+    GtkWidget *button = gtk_button_new_with_label("Submit");
+    g_signal_connect(button, "clicked", G_CALLBACK(on_back_to_main_menu), entry);
+    gtk_box_pack_start(GTK_BOX(main_box), button, TRUE, TRUE, 0);
+
+    GtkWidget *label = gtk_label_new("Data tidak ditemukan");
+    gtk_box_pack_start(GTK_BOX(main_box), label, TRUE, TRUE, 0);
+
+    GtkWidget *back_button = gtk_button_new_with_label("Kembali");
+    g_signal_connect(back_button, "clicked", G_CALLBACK(on_back_to_main_menu), NULL);
+    gtk_box_pack_start(GTK_BOX(main_box), back_button, TRUE, TRUE, 0);
+
+    gtk_widget_show_all(main_window);
+}
+
+void on_edit_riwayat_tambah_clicked(GtkWidget *widget, gpointer data) {
     clear_window();
 
     GtkWidget *entry = gtk_entry_new();
@@ -267,5 +365,4 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
-
 
